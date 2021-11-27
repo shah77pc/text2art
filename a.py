5a4b4db508af017fa12b09ddd3c4631e7ff33db8
shah77pc <73152402+shah77pc@users.noreply.github.com>
2021-11-27 06:41:36 -0800

    Update a.py

--- a.py
+++ a.py
@@ -1,2 +1,15 @@
-import sys
-sys.path.append("clipit")
+import clipit
+# To reset settings to default
+clipit.reset_settings()
+# You can use "|" to separate multiple prompts
+prompts = "underwater city"
+# You can trade off speed for quality: draft, normal, better, best
+quality = "normal"
+# Aspect ratio: widescreen, square
+aspect = "widescreen"
+# Add settings
+clipit.add_settings(prompts=prompts, quality=quality, aspect=aspect)
+# Apply these settings and run
+settings = clipit.apply_settings()
+clipit.do_init(settings)
+clipit.do_run(settings)
